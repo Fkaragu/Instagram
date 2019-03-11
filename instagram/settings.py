fcc0f8d2625774cdb206c0faa617de13b4a3c783
Francis Karagu <fkaragu@gmail.com>
2019-03-11 23:43:10 +0300

    Modified settings

--- instagram/settings.py
+++ instagram/settings.py
@@ -1,18 +1,8 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '^puq20+w#kx%46s&3su6y3a1*e4=3hiv&ij3q=2!6n1a6y^$1j'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
