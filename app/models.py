fe82dfe34b3e9c7c6e7115b25d1b5d2f8a072aaf
Francis Karagu <fkaragu@gmail.com>
2019-03-11 23:21:23 +0300

    Addec models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from pyuploadcare.dj.models import ImageField
 
-# Create your models here.
+class Profile(models.Model):
+    prof_pic = models.ImageField(default='default.png',upload_to='articles/', blank=True)
+    bio = models.CharField(max_length = 50)
+    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def search_profile(cls, name):
+        profile = Profile.objects.filter(user__username__icontains = name)
+        return profile
+
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(user = id)
+        return profile
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user = id).first()
+        return profile
+
+class Image(models.Model):
+    photo = models.ImageField(upload_to='articles/', blank=True)
+    image_name = models.CharField(max_length = 50)
+    image_caption = models.CharField(max_length = 50)
+    post_date = models.DateTimeField(auto_now=True)
+    likes = models.BooleanField(default=False)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-post_date',)
+
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def update_caption(cls, update):
+        pass
+
+    @classmethod
+    def get_image_id(cls, id):
+        image = Image.objects.get(pk=id)
+        return image
+
+    @classmethod
+    def get_profile_images(cls, profile):
+        images = Image.objects.filter(profile__pk = profile)
+        return images
+
+    @classmethod
+    def get_all_images(cls):
+        images = Image.objects.all()
+        return images
+
+class Comments(models.Model):
+    comment = models.CharField(max_length = 50)
+    posted_on = models.DateTimeField(auto_now=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save_comment(self):
+        self.save()
+
+    @classmethod
+    def get_comments_by_images(cls, id):
+        comments = Comments.objects.filter(image__pk = id)
+        return comments
